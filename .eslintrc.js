a332377b6072a791f31ad760d9691b1d2e60e30a
Rauli Laine <rauli.laine@iki.fi>
2017-01-06 18:18:09 +0200

    Add ESLint configuration
    Add ESLint configuration based on Airbnb JavaScript style guide, with
    some errors turned into warnings and stuff like "never use continue
    statement" turned off, since they are silly.

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": "airbnb",
+  "installedESLint": true,
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "rules": {
+    "no-continue": "off",
+    "no-loop-func": "off",
+    "no-param-reassign": "warn",
+    "no-unused-vars": "warn"
+  }
+};
